b0894b583abebad9da86b93edf56d721f287c32c
gulshan <gulshankumar0708@gmail.com>
2018-09-20 23:05:39 +0530

    setup

---
+++ booking/src/main/java/com/gk/booking/BookingApplication.java
@@ -0,0 +1,14 @@
+package com.gk.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+		
+		System.out.println("Running boot application");
+	}
+}
